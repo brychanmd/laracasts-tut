644c8ce980525d82c7fe31fcdc7e640bf233c89b
brychanmd <bmorrisdafydd@gmail.com>
2021-09-10 17:06:42 +0100

    working with DB finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Post::factory()->create();
+        $user = User::factory()->create(
+            [
+                'name' => 'John Doe'
+            ]
+        );
+
+        Post::factory(5)->create(
+            [
+                'user_id' => $user->id
+            ]
+        );
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,10 +7,10 @@
 		</h1>
 
 		<p>
-            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
-		<?php echo $post->body; ?>
+        <p>{{ $post->body }}</p>
 
 	</article>
 
