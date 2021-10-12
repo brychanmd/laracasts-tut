214796e7a12b8a7ac7d88a3048b4e64839db5476
brychanmd <bmorrisdafydd@gmail.com>
2021-10-12 20:06:39 +0100

    post page html

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,33 @@
 <x-layout>
 	
-		@foreach($posts as $post)
+		@include('_posts-header')
 
-		<article>
-			<h1>
-				<a href="/posts/{{ $post->slug }}">
-					{!! $post->title !!}
-				</a>
-			</h1>
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-			<p> By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+            @if( $posts->count() )
+                <x-post-featured-card :post="$posts[0]" />
 
-			<div>
-				{{ $post->excerpt }}
-			</div>
-		</article>
+                @if( 1 < $posts->count() )
+                    <div class="lg:grid lg:grid-cols-6">
+                    
+                        @foreach ( $posts->skip(1) as $post )
+                            <x-post-card 
+                            :post="$post" 
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" 
+                            />
+                        @endforeach
 
-    	@endforeach
+                    </div>
+                @endif
+            
+            @else
+
+                <p>Sorry, no posts yet..</p>
+            
+            @endif
+
+
+        </main>
 	
 </x-layout>
 
