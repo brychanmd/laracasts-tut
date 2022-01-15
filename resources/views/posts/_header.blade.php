bcc4a96183e152c78d1be0f1847f10aa42a1b627
brychanmd <bmorrisdafydd@gmail.com>
2022-01-15 15:11:30 +0000

    filtering and pagination

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -14,38 +14,11 @@
             <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
                 <!--  Category -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-                   
-                    <x-dropdown>
-
-                        <x-slot name="trigger">
-                            <button class="py-2 pl-3 pr-9 text-sm font-semibold w-32">{{ isset( $currentCategory ) ? ucwords( $currentCategory->name ) : 'Categories' }}</button>
-
-                            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px; bottom: 12px;" width="22"
-                            height="22" viewBox="0 0 22 22">
-                                <g fill="none" fill-rule="evenodd">
-                                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                    </path>
-                                    <path fill="#222"
-                                            d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                                </g>
-                            </svg>
-                        </x-slot>
-
-                        <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300">All</a>
-                                
-                        @foreach ($categories as $category)
-                            <a 
-                            href="/categories/{{ $category->slug }}" 
-                            class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300 {{ isset( $currentCategory ) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}"
-                            >{{ ucwords( $category->name ) }}</a>
-                        @endforeach
-
-                    </x-dropdown>    
-
+                   <x-category-dropdown />
                 </div>
 
                 <!-- Other Filters -->
-                <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+                {{-- <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
                     <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                         <option value="category" disabled selected>Other Filters
                         </option>
@@ -64,11 +37,16 @@
                                   d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
                         </g>
                     </svg>
-                </div>
+                </div> --}}
 
                 <!-- Search -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-                    <form method="GET" action="#">
+                    <form method="GET" action="/">
+
+                        @if (request('category'))
+                            <input type="hidden" name="category" value="{{request('category')}}">
+                        @endif
+
                         <input 
                         type="text" 
                         name="search" 
