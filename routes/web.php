65fc2b6060c0e5834cf90c637f320a2c7fbc8c04
brychanmd <bmorrisdafydd@gmail.com>
2022-01-21 00:09:30 +0000

    logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -29,5 +30,7 @@ Route::get('authors/{author:username}', function (User $author) {
 	]);
 });
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy']);
+// Route::post('login', [SessionsController::class, 'store']);
